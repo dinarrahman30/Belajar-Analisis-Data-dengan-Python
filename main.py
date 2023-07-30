93ce8ddaa4be80cf0d9e1015a694ba640c4a61e3
Dinar Wahyu Rahman <68122380+dinarrahman30@users.noreply.github.com>
2023-07-31 00:02:49 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from pathlib import Path
+
+def by_workingday(df):
+    byworkingday_bike = day_bike.groupby(by="workingday").instant.nunique().reset_index()
+    byworkingday_bike.rename(columns={"instant": "sum"}, inplace=True)
+    byworkingday_bike
+
+    return byworkingday_bike
+
+def by_weather(df):
+    byseason_bike = day_bike.groupby(by="weathersit").instant.nunique().reset_index()
+    byseason_bike.rename(columns={"instant": "sum"}, inplace=True)
+    byseason_bike
+
+    return byseason_bike
+
+with st.sidebar:
+    # Menambahkan logo perusahaan
+    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
+
+def workingday(df):
+    st.subheader("Working Day")
+    fig, ax = plt.figure(figsize=(10, 5))
+    sns.barplot(
+        y="sum",
+        x="workingday",
+        data=byworkingday_bike.sort_values(by="workingday", ascending=False),
+        ax=ax
+    )
+    ax.set_title("Bagaimana pengaruh hari kerja mempengeruhi pengunaan bike sharing?", loc="center", fontsize=15)
+    ax.set_ylabel("Jumlah Pengguna")
+    ax.set_xlabel(None)
+    ax.tick_params(axis="x", labelsize=20)
+    ax.tick_params(axis="y", labelsize=15)
+    st.pyplot(fig)
+
+def weather(df):
+    st.subheader("Weather")
+
+    fig, ax = plt.figure(figsize=(10, 5))
+    sns.barplot(
+        y="sum",
+        x="weathersit",
+        data=byseason_bike.sort_values(by="weathersit", ascending=False),
+    )
+    ax.set_title("Bagaimana kondisi cuaca terhadap banyaknya pengguna bike sharing?", loc="center", fontsize=15)
+    ax.set_ylabel("Jumlah Pengguna")
+    ax.set_xlabel(None)
+    ax.tick_params(axis="x", labelsize=20)
+    ax.tick_params(axis="y", labelsize=15)
+    st.pyplot(fig)
+
+
+with st.sidebar:
+    # Menambahkan logo perusahaan
+    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
+
+if __name__ == "__main__":
+    sns.set(style="white")
+
+    st.header("Bike Sharing Dashboard: bike: ")
+
+    day_bike_csv = Path(__file__).parents[1] / 'D:/dashboard.csv'
+
+    day_bike = pd.read_csv(day_bike_csv)
+
+#    date = sidebar(day_bike)
+#    if (len(date) == 2):
+#       main_bike = day_bike[(day_bike["dteday"] >= str(date[0])) & (day_bike["dteday"] <= str(date[1]))]
+#    else:
+#       main_bike = day_bike[
+#            (day_bike["dteday"] >= str(st.session_state.date[0])) & (day_bike["dteday"] <= str(st.session_state.date[1]))]
+
+#    workingday_bike = by_workingday(main_bike)
+#    workingday(workingday_bike)
+
+#    weathersit_bike = by_weather(main_bike)
+#    weathersit(weathersit_bike)
+
+    copyright = "Copyright © " + "2023" " | Bike Sharing Dashboard | All Rights Reserved | " + "Made by : by [@Dinar Wahyu Rahman](https://https://www.linkedin.com/in/dinar-wahyu-rahman-00a405162/)"
+    st.caption(copyright)
